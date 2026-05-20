76278e5766a0358fdc3bcae16187fb1be79837dd
Mohit Mourya <42367406+MohitMourya@users.noreply.github.com>
2026-05-20 08:27:23 +0530

    Update solution for precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -2,12 +2,19 @@ def precision_recall_at_k(recommended, relevant, k):
     """
     Compute precision@k and recall@k for a recommendation list.
     """
-    # Approach 1
-    hit_count = 0
-    for i in recommended[:k]:
-        if i in relevant:
-            hit_count += 1
+    # # Approach 1
+    # hit_count = 0
+    # for i in recommended[:k]:
+    #     if i in relevant:
+    #         hit_count += 1
 
-    precision = hit_count/k
-    recall = hit_count/len(relevant)
-    return [precision, recall]
+    # precision = hit_count/k
+    # recall = hit_count/len(relevant)
+    # return [precision, recall
+
+    # Approach 2
+    hit_count = sum(
+        recom in relevant
+            for recom in recommended[:k]
+    )
+    return [hit_count/k, hit_count/len(relevant)]
