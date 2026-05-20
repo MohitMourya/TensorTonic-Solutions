9c216b7e3ff450dc026c369e95217789e06e886b
Mohit Mourya <42367406+MohitMourya@users.noreply.github.com>
2026-05-20 08:17:29 +0530

    Update solution for hit-rate-at-k

--- hit-rate-at-k/hit-rate-at-k.py
+++ hit-rate-at-k/hit-rate-at-k.py
@@ -2,11 +2,19 @@ def hit_rate_at_k(recommendations, ground_truth, k):
     """
     Compute the hit rate at K.
     """
-    # Write code 
+    # # Basic Approach 
+    # hit_count = 0
+    # for i, recom in enumerate(recommendations):
+    #     if ground_truth[i][0] in recom[:k]:
+    #         hit_count +=1
+
+    # return hit_count/len(recommendations)
+
+    # Optimized Approach
     hit_count = 0
-    for i, recom in enumerate(recommendations):
-        if ground_truth[i][0] in recom[:k]:
-            hit_count +=1
+    for recom, gt in zip(recommendations, ground_truth):
+        if gt[0] in recom[:k]:
+            hit_count += 1
 
     return hit_count/len(recommendations)
 
